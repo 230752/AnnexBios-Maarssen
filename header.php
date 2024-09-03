10be923f5f5b38aaffa95f3df9edff5ea11b3cbd
230752 <230752@student.glu.nl>
2024-09-03 15:08:42 +0200

    begonnen aan index en header

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+    <div id="header-logo-container">
+        <img id="header-logo-img" src="assets/images/logo_hoofd.png" alt="web logo">
+    </div>
+    <div id="header-buttons">
+        <a class="header-button" href="">FILM AGENDA</a>
+        <a class="header-button" href="">ALLE VESTIGINGEN</a>
+        <a class="header-button" href="">CONTACT</a>
+    </div>
+</header>

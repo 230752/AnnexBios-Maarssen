bdcdfb719f832f4bc3a7a5a814e2a13d484c8a96
lism <LismHere@proton.me>
2024-09-05 15:39:09 +0200

    Page redirection added

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$pagePath = (include_once 'assets/modules/funcs/getRedirectPagePath.php')("pages/");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AnnexBios Maarssen</title>
-    <!-- CSS LINKS -->
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <!-- FONT LINKS -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-        rel="stylesheet">
-</head>
-
-<body>
-    <div id="container">
-        <?php include "assets/php/header.php" ?>
-        <main>
-            <h1>content</h1>
-        </main>
-        <?php include "assets/php/footer.php" ?>
-    </div>
-</body>
-
-</html>
+include_once $pagePath;
+?>

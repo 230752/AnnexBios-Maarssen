a286d3af1d53dcb08ca80732fbbfb902f2919b63
lism <LismHere@proton.me>
2024-09-05 16:00:11 +0200

    Removed echo in getRedicrectPagePath that would offset the top

--- assets/modules/funcs/getRedirectPagePath.php
+++ assets/modules/funcs/getRedirectPagePath.php
@@ -3,8 +3,6 @@ return function ($startPath) {
     $pageName = isset($_GET['p']) ? $_GET['p'] : 'home';
     $path = $startPath . $pageName . '.php';
 
-    echo file_exists($path);
-
     if (!file_exists($path)) {
         return $startPath . '404.php';
     }

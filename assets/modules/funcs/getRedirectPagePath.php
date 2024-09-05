91f8648a9bd2f15c1fb6d64f6221e717e50d01cc
lism <LismHere@proton.me>
2024-09-05 16:12:01 +0200

    Information on the getredirectpagepath function

--- assets/modules/funcs/getRedirectPagePath.php
+++ assets/modules/funcs/getRedirectPagePath.php
@@ -1,4 +1,11 @@
 <?php
+// 
+// 
+// Args: 
+//   - $startPath: string - path to start with so for example you put in "assets/" it'll check inside of assets folder
+//
+// Returns: string - path to the page
+
 return function ($startPath) {
     $pageName = isset($_GET['p']) ? $_GET['p'] : 'home';
     $path = $startPath . $pageName . '.php';

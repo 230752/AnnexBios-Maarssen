e39a2d9d756a7efbdfe08bd234fd2245ba3aae1c
Jos <p1pm@pm.me>
2024-09-06 15:56:47 +0200

    Fixed Footer logo at the right

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,42 +1,49 @@
-<style>
-
-</style>
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 <footer>
-    <div id="logo">
-    <img src="assets/images/logos/logo.png">
-    <p id="textfooter">Lorem ipsum dolor sit amet, consectetur<span class="line-break"></span>
-    adipiscing elit. Aenean commodo ligula eget<span class="line-break"></span>
-    dolor. Aenean massa. Cum sociis natoque<span class="line-break"></span>
-    <span id="hide"></span><span id="more">
-    penatibus et magnis dis parturient montes,<span class="line-break"></span>
-    nascetur ridiculus mus. Donec quam felis,<span class="line-break"></span>
-    ultricies nec, pellentesque eu, pretium quis, sem.
-</p>
-<button onclick="leesMeer()" id="myBtn">LEES MEER</button>
+    <div class="footer-container">
+        <div id="logo">
+        <img src="assets/images/logos/logo.png">
+        <p id="textfooter">
+            Lorem ipsum dolor sit amet, consectetur
+            
+            <span class="line-break"></span>
+            adipiscing elit. Aenean commodo ligula eget
+            <span class="line-break"></span>
+            dolor. Aenean massa. Cum sociis natoque
+            <span class="line-break"></span>
+            <span id="hide"></span>
+            <span id="more">
+            penatibus et magnis dis parturient montes,
+            <span class="line-break"></span>
+            nascetur ridiculus mus. Donec quam felis,
+            <span class="line-break"></span>
+            ultricies nec, pellentesque eu, pretium quis, sem.
+        </p>
+        <button onclick="leesMeer()" id="myBtn">LEES MEER</button>
+            </div>
+
+            <div id="navigate">
+                <h2>NAVIGEER</h2>
+                <p>Werken bij</p>
+                <p>Veelgestelde vragen</p>
+                <p>Vestigingen</p>
+                <p>Contact</p>
+        </div>
+
+        <div id="volgons">
+            <h2>VOLG ONS</h2>
+            <div class="social-buttons">
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-instagram"></a>
+            </div>
+        </div>
     </div>
+    
 
-    <div id="navigate">
-        <h2>NAVIGEER</h2>
-        <p>Werken bij</p>
-        <p>Veelgestelde vragen</p>
-        <p>Vestigingen</p>
-        <p>Contact</p>
-</div>
-
-<div id="volgons">
-    <h2>VOLG ONS</h2>
-    <div class="social-buttons">
-        <a href="#" class="fa fa-facebook"></a>
-        <a href="#" class="fa fa-twitter"></a>
-        <a href="#" class="fa fa-instagram"></a>
-    </div>
-</div>
-
-<img src="assets/images/backgrounds/biosladder.png" id="logo2">
+    <img src="assets/images/backgrounds/biosladder.png" id="logo2">
 </footer>
 
 

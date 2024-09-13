b9d526ac1ee3a48ac4d514fb007b7e82470bb8d8
230752 <230752@student.glu.nl>
2024-09-13 15:42:25 +0200

    kleine verbeteringen

--- pages/vestigingen.php
+++ pages/vestigingen.php
@@ -7,22 +7,29 @@
     <title>AnnexBios Maarssen</title>
     <!-- CSS LINKS -->
     <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/vestigingen.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
     <!-- FONT LINKS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
         rel="stylesheet">
+    <!-- ICON LINKS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <div id="container">
         <?php include "assets/php/header.php" ?>
         <main>
-            <h1>content</h1>
+            <div id="test">
+                <h1>test</h1>
+            </div>
         </main>
         <footer>
-            <h1>footer</h1>
+            <?php include "assets/php/footer.php" ?>
         </footer>
     </div>
 </body>
